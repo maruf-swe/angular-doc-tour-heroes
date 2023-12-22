1e73025ddfb0b439d2ddca05e49f55fd84c7c4b0
maruf-swe <marufur1847@gmail.com>
2023-12-22 11:42:44 +0600

    add app css files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-doc-tour-heroes';
+  title = 'Angular Tour of Heroes';
 }
