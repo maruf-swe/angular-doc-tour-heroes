67eb13ddd12054adcf28d55ee49713d27bdff030
maruf-swe <marufur1847@gmail.com>
2023-12-23 23:09:36 +0600

    add heroes componenet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Angular Tour of Heroes';
+  title = 'Tour of Heroes';
 }
