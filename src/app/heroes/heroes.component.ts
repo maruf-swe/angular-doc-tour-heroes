c40482c81b2a77fb64af8cec1230dfeb1f2a2a14
maruf-swe <marufur1847@gmail.com>
2023-12-24 23:46:21 +0600

    add mock-heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,19 +2,18 @@ import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HEROES } from '../mock-heroes';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, NgFor],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Maruf'
-  }
+  heroes = HEROES;
 
 }
