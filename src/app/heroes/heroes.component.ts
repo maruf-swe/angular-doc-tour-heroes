f841193aeed45ed53cacc05ffad7e93608253c5c
maruf-swe <marufur1847@gmail.com>
2023-12-25 23:13:19 +0600

    create service and use it

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { FormsModule } from '@angular/forms';
 import { HEROES } from '../mock-heroes';
 import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { HeroDetailsComponent } from '../hero-details/hero-details.component';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,13 +13,26 @@ import { HeroDetailsComponent } from '../hero-details/hero-details.component';
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit{
 
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
+
+  constructor(private heroService: HeroService){}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
   
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
 }
+
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+}
+
 
 }
