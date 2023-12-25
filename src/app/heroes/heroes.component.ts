7c467004147af3c751706ed1a1c07db1b95e7a7b
maruf-swe <marufur1847@gmail.com>
2023-12-25 20:38:08 +0600

    update heroes componenet file

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HEROES } from '../mock-heroes';
 import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
@@ -8,7 +7,7 @@ import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [BrowserModule, FormsModule, NgIf, NgFor, UpperCasePipe],
+  imports: [ FormsModule, NgIf, NgFor, UpperCasePipe],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
@@ -17,7 +16,7 @@ export class HeroesComponent {
   heroes = HEROES;
 
   selectedHero?: Hero;
-onSelect(hero: Hero): void {
+  onSelect(hero: Hero): void {
   this.selectedHero = hero;
 }
 
