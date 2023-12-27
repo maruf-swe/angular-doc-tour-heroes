e3acfbdda205a4d74b0d53b5fdfa94e1be7cbe99
maruf-swe <marufur1847@gmail.com>
2023-12-27 23:14:38 +0600

    add message service functionally

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -17,6 +17,7 @@ import { MessageService } from '../message.service';
 export class HeroesComponent implements OnInit{
 
   heroes: Hero[] = [];
+  
   selectedHero?: Hero;
 
   constructor(private heroService: HeroService, private messageService: MessageService){
