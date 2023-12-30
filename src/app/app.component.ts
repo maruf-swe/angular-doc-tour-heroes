f4f861f0ef64b9d49b1cb7177d5e5295e1e28380
maruf-swe <marufur1847@gmail.com>
2023-12-30 23:47:03 +0600

    Add router outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeroesComponent, MessagesComponent],
+  imports: [CommonModule, RouterOutlet, HeroesComponent, MessagesComponent,AppRoutingModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
