608ee0b5c8a9f68a4c9a5db78e966a9d0c6e007e
maruf-swe <marufur1847@gmail.com>
2024-01-02 04:52:25 +0600

    add hero details

--- src/app/hero-details/hero-details.component.ts
+++ src/app/hero-details/hero-details.component.ts
@@ -1,17 +1,38 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { NgIf, UpperCasePipe } from '@angular/common';
+import { CommonModule, Location, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-details',
   standalone: true,
-  imports: [NgIf, UpperCasePipe, FormsModule],
+  imports: [CommonModule, UpperCasePipe, FormsModule],
   templateUrl: './hero-details.component.html',
   styleUrl: './hero-details.component.css'
 })
-export class HeroDetailsComponent {
+export class HeroDetailsComponent implements OnInit {
  
-@Input() hero?: Hero;
+hero: Hero | undefined;
+
+constructor(
+  private route: ActivatedRoute,
+  private heroService: HeroService,
+  private location: Location
+){}
+
+ngOnInit(): void {
+  this.getHero();
+}
+
+getHero():void{
+  const id = Number(this.route.snapshot.paramMap.get('id'));
+  this.heroService.getHero(id).subscribe(hero => this.hero = hero);
+}
+
+goBack():void{
+  this.location.back();
+}
 
 }
