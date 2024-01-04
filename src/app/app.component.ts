a718b2c6ef1e39e194c9e91cc0a2b2ecfa6c0717
maruf-swe <marufur1847@gmail.com>
2024-01-04 23:56:59 +0600

    delete route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,15 +5,13 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailsComponent } from './hero-details/hero-details.component';
-import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeroesComponent, HeroDetailsComponent,
-            MessagesComponent, DashboardComponent, HttpClientModule, AppRoutingModule,
-             RouterModule
+  imports: [CommonModule, HeroesComponent, HeroDetailsComponent,
+            MessagesComponent, DashboardComponent, HttpClientModule,
      ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
